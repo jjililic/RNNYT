eb434ab8d775e4cf85bf6a7ac839fa762c508810
Mingyu, Ji <melvin.ji@gmail.com>
2018-05-16 16:35:25 +0900

    'Title' 함수형 컴포넌트 추가

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,9 @@ import AppText from './AppText';
 import * as globalStyles from '../styles/global';
 
 const Title = ({ style, children }) => (
-    <AppText/>
+    <AppText style={[styles.title, style]}>
+        {children}
+    </AppText>
 );
 
 Title.propTypes = {
