f9d8be96c5e5bb95f84314b9991fdaa9a0d6a72b
Mingyu, Ji <melvin.ji@gmail.com>
2018-05-18 12:06:39 +0900

    'AppText' 컴포넌트 버그 수정

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -4,12 +4,12 @@ import { Text } from 'react-native';
 import * as globalStyles from '../styles/global';
 
 const AppText = ({ children, style, ...rest }) => (
-    <Text style={[globalStyles.COMMON_STYLES, style]} {...rest}>
+    <Text style={[globalStyles.COMMON_STYLES.text, style]} {...rest}>
         {children}
     </Text>
 );
 
-AppText.prototype = {
+AppText.propTypes = {
     style: Text.propTypes.style,
     children: PropTypes.node
 };
